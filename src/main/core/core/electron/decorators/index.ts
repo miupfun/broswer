0220a431c650cb0a81bbe8d73f500b23c89a273e
TristanWong <tw949561391@icloud.com>
2021-01-11 11:43:58 +0800

    浏览器组件

--- src/main/core/core/electron/decorators/index.ts
+++ src/main/core/core/electron/decorators/index.ts
@@ -3,34 +3,34 @@ import {MiupApplication} from "../../ioc";
 
 
 export function MpAppOn(event: string, op?: { once: boolean }) {
-    return (target: any, methodName: string, desc: PropertyDescriptor) => {
-        if (op && op.once) {
-            app.once(event as any, () => {
-                const who = MiupApplication.container.get(target.constructor)
-                desc.value.apply(who)
-            })
-        } else {
-            app.on(event as any, () => {
-                const who = MiupApplication.container.get(target.constructor)
-                desc.value.apply(who)
-            })
-        }
+  return (target: any, methodName: string, desc: PropertyDescriptor) => {
+    if (op && op.once) {
+      app.once(event as any, () => {
+        const who = MiupApplication.container.get(target.constructor)
+        desc.value.apply(who)
+      })
+    } else {
+      app.on(event as any, () => {
+        const who = MiupApplication.container.get(target.constructor)
+        desc.value.apply(who)
+      })
     }
+  }
 }
 
 export function MpIpcMainOn(event: string, op?: { once: boolean }) {
-    return (target: any, methodName: string, desc: PropertyDescriptor) => {
-        if (op && op.once) {
-            ipcMain.once(event as any, (e: Event, ...params) => {
-                const who = MiupApplication.container.get(target.constructor)
-                desc.value.apply(who, e, ...params)
-            })
-        } else {
-            ipcMain.on(event as any, (e: Event, ...params) => {
-                const who = MiupApplication.container.get(target.constructor)
-                desc.value.call(who, e, ...params)
-            })
-        }
+  return (target: any, methodName: string, desc: PropertyDescriptor) => {
+    if (op && op.once) {
+      ipcMain.once(event as any, (e: Event, ...params) => {
+        const who = MiupApplication.container.get(target.constructor)
+        desc.value.apply(who, e, ...params)
+      })
+    } else {
+      ipcMain.on(event as any, (e: Event, ...params) => {
+        const who = MiupApplication.container.get(target.constructor)
+        desc.value.call(who, e, ...params)
+      })
     }
+  }
 }
 
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,50 +1,59 @@
 import {app, BrowserView, BrowserWindow, ipcMain} from 'electron';
 import {is} from 'electron-util';
 import {RouteUtil} from "../share/utils/route.util";
+import {bootstrap} from "./main";
 
-app.on('ready', () => {
-  const headerHeight = 36
-  const w = new BrowserWindow({
-    show: false,
-    frame: !is.windows,
-    titleBarStyle: "hiddenInset",
-    width: 1280,
-    height: 720,
-    minWidth: 480,
-    minHeight: 480,
-    center: true,
-    fullscreen:true,
-    backgroundColor: '#fff',
-    webPreferences: {
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      nodeIntegrationInSubFrames: false,
-      nodeIntegrationInWorker: true,
-      contextIsolation: false,
-      webviewTag: true,
-    }
-  });
-  w.loadURL(RouteUtil.getPageUrl('browser')).then(() => {
-    w.show();
+// app.on('ready', () => {
+//   const headerHeight = 36
+//   const w = new BrowserWindow({
+//     show: false,
+//     frame: !is.windows,
+//     titleBarStyle: "hiddenInset",
+//     width: 1280,
+//     height: 720,
+//     minWidth: 480,
+//     minHeight: 480,
+//     center: true,
+//     fullscreen:true,
+//     backgroundColor: '#fff',
+//     webPreferences: {
+//       enableRemoteModule: true,
+//       nodeIntegration: true,
+//       nodeIntegrationInSubFrames: false,
+//       nodeIntegrationInWorker: true,
+//       contextIsolation: false,
+//       webviewTag: true,
+//     }
+//   });
+//   w.loadURL(RouteUtil.getPageUrl('browser')).then(() => {
+//     w.show();
+//
+//
+//     const view = new BrowserView()
+//     view.setBounds({x: 0, y: headerHeight, width: w.getBounds().width, height: w.getBounds().height - headerHeight})
+//     view.setAutoResize({
+//       width:true,
+//       height:true,
+//       vertical:false,
+//       horizontal:false
+//     })
+//     w.setBrowserView(view)
+//     view.webContents.loadURL('https://angular.io')
+//     view.webContents.toggleDevTools()
+//   });
+// });
+// app.on('window-all-closed', () => {
+//   app.quit()
+//   app.exit()
+// })
+// ipcMain.on('create-new-Tab', () => {
+//
+// })
 
-
-    const view = new BrowserView()
-    view.setBounds({x: 0, y: headerHeight, width: w.getBounds().width, height: w.getBounds().height - headerHeight})
-    view.setAutoResize({
-      width:true,
-      height:true,
-      vertical:false,
-      horizontal:false
-    })
-    w.setBrowserView(view)
-    view.webContents.loadURL('https://angular.io')
-    view.webContents.toggleDevTools()
-  });
-});
-app.on('window-all-closed', () => {
+if (app.requestSingleInstanceLock()) {
   app.quit()
-  app.exit()
-})
-ipcMain.on('create-new-Tab', () => {
-
-})
+} else {
+  import('./main').then(m => {
+    m.bootstrap().then()
+  })
+}
